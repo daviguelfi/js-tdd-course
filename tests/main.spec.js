030394ed270bc0ab05529b23fab10edb7c2bb484
daviguelfi <daviguelfi@gmail.com>
2020-05-26 16:06:19 -0300

    create a basic test structure

---
+++ tests/main.spec.js
@@ -0,0 +1,11 @@
+describe('Main', function() { //descreve o teste de um certo arquivo
+  describe('Method A', function() { 
+    context('Case 1', function() { //separa por caso de teste
+      it('should happen bla', function() { //bloco onde de fato executa os testes
+        //espera que aconteça
+        // entrada de dados / metodo sum(2, 2)
+        // espera retornar (4) => true | (3) => false
+      })
+    })
+  })
+});
